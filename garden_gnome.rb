4768999b70cb96f191631ca1c40a1c999f372759
Zoe Dumas <zoe.dumas1@yahoo.com>
2018-06-22 16:30:29 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -51,6 +51,6 @@ class GardenGnome
 
 
   def introduce_self
-    puts "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
+    return "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
   end
 end
